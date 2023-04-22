b0b5d67f3a6bf3929b353cb32ce462c5a0a0ac28
tarikdikkaya <tarikdikkaya@gmail.com>
2023-04-22 13:57:25 +0300

    uploaded py file

---
+++ asalSayiSorgu.py
@@ -0,0 +1,24 @@
+# Kullanıcıdan bir sayı girişi yapmasını istiyoruz.
+girilenSayi = int(input("Asal olup olmadığını kontrol etmek istediğiniz sayıyı giriniz: "))
+
+# Girilen sayıya tam bölünen sayıları kaydetmek için bir liste oluşturuyoruz.
+bolunenler = []
+
+while True:
+    # Girilen sayının 2'den kendisine kadar bütün sayılara tek tek böldürüyoruz, 
+    # tam bölünen sayıları 'bolunenler' listesine ekletiyoruz.
+    for i in range (2,girilenSayi):
+        if girilenSayi % i == 0:
+            bolunenler.append(i)
+
+    # 'bolunenler' listesinin uzunluğu 0'a eşit değilse yani sayının tam bölüneni varsa
+    # kullanıcıya sayının asal olmadığını bildiriyoruz. 
+    if len(bolunenler) != 0:
+        print(f"{girilenSayi} asal değil. {bolunenler} sayılarına bölünüyor.")
+        break
+    
+    # Eğer 'bolunenler' listesi 0'a eşit ise yani sayının tam böleni yoksa kullanıcıya 
+    # sayının asal olduğunu bildiriyoruz.
+    else:
+        print(f"{girilenSayi} asal.")
+        break
